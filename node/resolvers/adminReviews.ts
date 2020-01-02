665df6afcd25b4168cd8f800d7c1e366798e65fa
amartinwebimpacto <amartin@webimpacto.es>
2020-01-02 15:24:33 +0100

    VTEX-4 Modificada peticion search por scroll. Usar filtros en el cliente de vtex

--- node/resolvers/adminReviews.ts
+++ node/resolvers/adminReviews.ts
@@ -11,10 +11,8 @@ interface Args {
 }
 
 export async function adminReviews(_root: any, args: Args, ctx: any) {
-    const pageItems = 99;
-
-    let URL = `http://${ctx.vtex.account}.myvtex.com/api/dataentities/RE/search?_fields=id,reviewId,approved,comment,locale,name,productId,score&_where=` ;
-
+    let URL = `http://${ctx.vtex.account}.myvtex.com/api/dataentities/RE/scroll?_fields=id,reviewId,approved,comment,locale,name,productId,score&_where=` ;
+    
     // Filter by productId
     if(typeof args["productId"] == "undefined"){
         URL += 'productId<>0'        
@@ -30,23 +28,16 @@ export async function adminReviews(_root: any, args: Args, ctx: any) {
             URL += ' AND approved=false'
         }
     }
+    
     // Filter by locale
     if(typeof args["locale"] != "undefined" && args["locale"]!=''){
         URL += ' AND locale=' + args["locale"]
     }
-    //URL += '&fq=P:[0 TO 200]'
-    // Filter by range
-    if(typeof args["from"] == "undefined"){
-        args["from"] = 0;
-    }
-    if(typeof args["to"] == "undefined"){
-        args["to"] = pageItems;
-    }
+    
     let response = await axios({
         headers: {
             'VtexIdclientAutCookie': ctx.vtex.authToken,
-            'Proxy-Authorization': ctx.vtex.authToken,
-            'REST-Range': 'resources=' + args["from"] + '-' + args["to"]
+            'Proxy-Authorization': ctx.vtex.authToken
         },
         method: 'GET',
         url: URL,
@@ -55,8 +46,8 @@ export async function adminReviews(_root: any, args: Args, ctx: any) {
             return review;
         })
     }).catch(function (error) {
-        console.log("ERROR"); 
-        console.log(error);
+        console.log("-------> ERROR"); 
+        console.log(error.response.data);
     });
 
     return response;
--- react/AdminReviews.tsx
+++ react/AdminReviews.tsx
@@ -10,22 +10,28 @@ import { withRuntimeContext } from 'vtex.render-runtime'
 import { Table } from 'vtex.styleguide'
 import { Checkbox } from 'vtex.styleguide'
 import { Input } from 'vtex.styleguide'
-import { FilterBar } from 'vtex.styleguide'
 
-//const Checkbox = require('../Checkbox').default
 interface VtexFunctionComponent extends FunctionComponent {
   getSchema?(props: any): {}
 }
 
-//import emptyStarDefault from './img/star-empty.png'
-//import emptyFullDefault from './img/star-yellow.png'
 const AdminReviews: VtexFunctionComponent = (props: any) => {
-  const { /*colorStars,*/ starsType } = props
-  const [filterProductId, setFilterProductId] = useState(props.query.productId ? props.query.productId : '');
-  //const [filterLocale, setFilterLocale] = useState(props.query.locale ? props.query.locale : '');
-  const [filterApproved, setFilterApproved] = useState(props.query.approved ? props.query.approved : '');
-  const [filterNotApproved, setFilterNotApproved] = useState(props.query.notapproved ? props.query.notapproved : '');
-  
+  const { starsType } = props
+  const [filterStatements, setFilterStatements] = useState(props.query.filterStatements ? props.query.filterStatements : []);
+  const [tableLength] = useState(10);
+  const [currentPage,setCurrentPage] = useState(1);
+  const [sampleData, setSampleData] = useState(props.data.adminReviews);
+  const [slicedData,setSlicedData] = useState(props.data.adminReviews ? props.data.adminReviews.slice(0, tableLength): []);
+  const [currentItemFrom,setCurrentItemFrom] = useState(1);
+  const [currentItemTo,setCurrentItemTo] = useState(tableLength);
+  const [itemsLength, setItemsLength] = useState(props.data.adminReviews?props.data.adminReviews.length:999999999999);
+  const [emptyStateLabel] = useState('Nothing to show.');
+
+if(props.data.adminReviews && itemsLength==999999999999) {
+  setItemsLength(props.data.adminReviews.length);
+  setSlicedData(props.data.adminReviews.slice(0, tableLength));
+  setSampleData(props.data.adminReviews);
+}
   let ratingDynamicProps:RatingComponentProps = {}
   if(starsType == 'Custom Image'){
     let { imageStarsEmpty, imageStarsFilled } = props;
@@ -41,7 +47,6 @@ const AdminReviews: VtexFunctionComponent = (props: any) => {
     ratingDynamicProps.emptySymbol = `${styles.star} ${styles['star--empty']} ${fontAwesome.fa} ${fontAwesome['fa-star-o']}`
     ratingDynamicProps.fullSymbol = `${styles.star} ${styles['star--filled']} ${fontAwesome.fa} ${fontAwesome['fa-star']}`
   }
-
   const defaultSchema = {
     properties: {
       productId: {
@@ -96,21 +101,116 @@ const AdminReviews: VtexFunctionComponent = (props: any) => {
     },
   }
 
-  const [tableLength] = useState(10);
-  const [currentPage,setCurrentPage] = useState(1);
-  const [slicedData,setSlicedData] = useState(props.data.adminReviews ? props.data.adminReviews.slice(0, tableLength): []);
-  const [currentItemFrom,setCurrentItemFrom] = useState(1);
-  const [currentItemTo,setCurrentItemTo] = useState(tableLength);
-  const [itemsLength, setItemsLength] = useState(props.data.adminReviews?props.data.adminReviews.length:999999999999);
-  const [emptyStateLabel] = useState('Nothing to show.');
-  const [statements,setStatements] = useState([]);
-  
-  // La primera carga de la app a veces fallaba. Con esto se soluciona:
-  if(props.data.adminReviews && itemsLength==999999999999) {
-    setItemsLength(props.data.adminReviews.length);
-    setSlicedData(props.data.adminReviews.slice(0, tableLength));
+  function simpleInputObject({ value, onChange }:any) {
+    return (
+      <Input value={value || ''} onChange={(e:any) => onChange(e.target.value)} />
+    )
   }
-  
+
+  function simpleInputVerbsAndLabel() {
+    return {
+      renderFilterLabel: (st:any) => {
+        if (!st || !st.object) {
+          // you should treat empty object cases only for alwaysVisibleFilters
+          return 'Any'
+        }
+        return `${
+          st.verb === '=' ? 'is' : st.verb === '!=' ? 'is not' : 'contains'
+        } ${st.object}`
+      },
+      verbs: [
+        {
+          label: 'is',
+          value: '=',
+          object: simpleInputObject,
+        },
+      ],
+    }
+  }
+
+  function approvedSelectorObject({ value, onChange }:any) {
+    const initialValue = {
+      approved: true,
+      notapproved: true,
+      ...(value || {}),
+    }
+    const toggleValueByKey = (key:any) => {
+      const newValue = {
+        ...(value || initialValue),
+        [key]: value ? !value[key] : false,
+      }
+      return newValue
+    }
+    return (
+      <div>
+        {Object.keys(initialValue).map((opt, index) => {
+          return (
+            <div className="mb3" key={`class-statment-object-${opt}-${index}`}>
+              <Checkbox
+                checked={value ? value[opt] : initialValue[opt]}
+                label={opt}
+                name="default-checkbox-group"
+                onChange={() => {
+                  const newValue = toggleValueByKey(`${opt}`)
+                  const newValueKeys = Object.keys(newValue)
+                  const isEmptyFilter = !newValueKeys.some(
+                    key => !newValue[key]
+                  )
+                  onChange(isEmptyFilter ? null : newValue)
+                }}
+                value={opt}
+              />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  function handleFiltersChange(statements = []) {
+    let newData = Object.values(sampleData);
+    statements.forEach((st:any) => {
+      if (!st || !st.object) return 
+      const { subject, verb, object } = st
+      switch (subject) {        
+        case 'approved':
+          if (!object) return
+          newData = newData.filter(function(item:any){
+            if(object["approved"] && item.approved){
+              return item;
+            }
+            if(object["notapproved"] && !item.approved){
+              return item;
+            }
+          })
+          break
+        case 'productId':
+        case 'locale':
+          newData = newData.filter(function(item:any){ 
+            return item[subject] == object;
+          })
+          break
+        case 'number':
+          if (verb === '=') {
+            newData = newData.filter((item:any) => item.number === parseInt(object))
+          } else if (verb === 'between') {
+            newData = newData.filter(
+              (item:any) =>
+                item.number >= parseInt(object.first) &&
+                item.number <= parseInt(object.last)
+            )
+          }
+          break
+      }
+    })
+    const newDataLength = newData.length
+    const newSlicedData = newData.slice(0, tableLength)
+    setFilterStatements(statements);
+    setSlicedData(newSlicedData)
+    setItemsLength(newDataLength)
+    setCurrentItemTo(tableLength > newDataLength ? newDataLength : tableLength)
+  }
+
   function handleNextClick() {
     const newPage = currentPage + 1
     const itemFrom = currentItemTo + 1
@@ -133,251 +233,9 @@ const AdminReviews: VtexFunctionComponent = (props: any) => {
     setSlicedData(slicedData);
   }
 
-  function SimpleInputObject( {value, onChange}:any) {
-    return (
-      <Input value={value || ''} onChange={(e:any) => onChange(e.target.value)} />
-    )
-  }
-  /*function simpleInputVerbsAndLabel() {
-    return {
-      renderFilterLabel: (st:any) => {
-        if (!st || !st.object) {
-          console.log("Devuelvo 'Any'");
-          // you should treat empty object cases only for alwaysVisibleFilters
-          return 'Any'
-        }
-        return `${
-          st.verb === '=' ? 'is' : st.verb === '!=' ? 'is not' : 'contains'
-        } ${st.object}`
-      },
-      verbs: [
-        {
-          label: 'is',
-          value: '=',
-          object: simpleInputObject,
-        },
-        {
-          label: 'is not',
-          value: '!=',
-          object: simpleInputObject,
-        },
-        {
-          label: 'contains',
-          value: 'contains',
-          object: simpleInputObject,
-        },
-      ],
-    }
-  }*/
-
-
-  /*function filtrar(){
-    let URLQuery = '';
-    
-    let productId = (document.getElementById("review_filter_productId") as HTMLInputElement).value;
-    if(!isNaN(parseInt(productId))){
-      URLQuery = URLQuery + '&productId=' + productId;
-    } 
-
-    let locale = (document.getElementById("review_filter_locale") as HTMLInputElement).value;
-    if(locale != ''){
-      URLQuery = URLQuery + '&locale=' + locale;
-    }
-    
-    let approved = document.getElementById("review_filter_approved") as HTMLInputElement;  
-    let notapproved = document.getElementById("review_filter_notapproved") as HTMLInputElement;
-    if(!approved.checked || !notapproved.checked){
-      if(approved.checked){
-        URLQuery = URLQuery + '&approved=true';
-      }    
-      if(notapproved.checked){
-        URLQuery = URLQuery + '&notapproved=true';
-      }
-    }
-
-    props.runtime.navigate({
-      page: 'admin.app.admin-reviews',
-      query: ''+URLQuery+''
-    });
-  }*/
-  function filtrar(statements_: any){
-    let URLQuery = '';
-    
-    if(statements_){
-      setStatements(statements_);
-    }
-    
-    statements_.map((el:any)=>{
-      switch(el.subject) { 
-        case "product": {
-          setFilterProductId((el.object) ? el.object : filterProductId);
-          let productId = el.object;
-          if(!isNaN(parseInt(productId))){
-            URLQuery = URLQuery + '&productId=' + productId;
-          } 
-          break;  
-        } 
-        case "approved": {
-          setFilterNotApproved((el.object["not approved"]) ? el.object["not approved"] : filterNotApproved);
-          setFilterApproved((el.object["approved"]) ? el.object["approved"] : filterApproved);
-          let filterApprovedAux = el.object["approved"];
-          let filterNotApprovedAux = el.object["not approved"];
-          if(!filterApprovedAux || !filterNotApprovedAux){
-            if(filterApprovedAux){
-              URLQuery = URLQuery + '&approved=true';
-            }
-            if(filterNotApprovedAux){
-              URLQuery = URLQuery + '&notapproved=true';
-            }
-          }
-          break; 
-        } 
-        default: {
-          break; 
-        } 
-     }
-    });
-
-    /*let locale = (document.getElementById("review_filter_locale") as HTMLInputElement).value;
-    if(locale != ''){
-      URLQuery = URLQuery + '&locale=' + locale;
-    }*/
-
-    props.runtime.navigate({
-      query: ''+URLQuery+'',
-      page: 'admin.app.admin-reviews',
-    });
-   
-    setTimeout(() => {
-      location.reload();
-    }, 100);
-  }
-
-  function ApprovedSelectorObject({ value, error, onChange }:any) {
-    const initialValue = {
-      "approved": true,
-      "not approved": true,
-      ...(value || {}),
-    }
-    const toggleValueByKey = (key:any) => {
-      const newValues = {
-        ...(value || initialValue),
-        [key]: value ? !value[key] : false,
-      }
-      return newValues
-    }
-    return (
-      <div>
-        {Object.keys(initialValue).map((opt, index) => {
-          return (
-            <div className="mb3" key={`class-statment-object-${opt}-${index}`}>
-              <Checkbox
-                checked={value ? value[opt] : initialValue[opt]}
-                id={`class-${opt}`}
-                label={opt}
-                name="class-checkbox-group"
-                onChange={() => {
-                  const newValue = toggleValueByKey(`${opt}`)
-                  onChange(newValue)
-                }}
-                value={opt}
-              />
-            </div>
-          )
-        })}
-      </div>
-    )
-    console.log(error);
-  }
-  
   return (
     <div className="w-100 ph3 ph5-m ph2-xl mw9 mt4">  {/* Resumen de reviews */}
       <div className={"f6 w-100 mw8 center"}>
-        <FilterBar
-          alwaysVisibleFilters={['product', 'approved']}
-          statements={statements}
-          onChangeStatements={(statements_:any) => filtrar(statements_)}
-          clearAllFiltersButtonLabel="Clear Filters"
-          options={{
-            product: {
-              label: 'Product',
-              renderFilterLabel: (st:any) => {
-                if (!st || !st.object) {
-                  // you should treat empty object cases only for alwaysVisibleFilters
-                  return 'Any'
-                }
-                return `${
-                  st.verb === '='
-                    ? 'is'
-                    : st.verb === '!='
-                    ? 'is not'
-                    : 'contains '
-                } ${st.object}`
-              },
-              verbs: [
-                /*{
-                  label: 'contains',
-                  value: 'contains',
-                  object: (props:any) => <SimpleInputObject {...props} />,
-                },*/
-                {
-                  label: 'is',
-                  value: '=',
-                  object: (props:any) => <SimpleInputObject {...props} />,
-                }/*,
-                {
-                  label: 'is not',
-                  value: '!=',
-                  object: (props:any) => <SimpleInputObject {...props} />,
-                },*/
-              ],
-            },
-            approved: {
-              label: 'Approved',
-              renderFilterLabel: (st:any) => {
-                if (!st || !st.object) {
-                  // you should treat empty object cases only for alwaysVisibleFilters
-                  return 'All'
-                }
-                
-                //const keys = st.object ? Object.keys(st.object) : {}
-                //return 'All'// Esta linea hay que borrarla
-                
-                const isAllTrue = st.object["approved"] && st.object["not approved"]
-                const isAllFalse = !st.object["approved"] && !st.object["not approved"]
-               
-                //const trueKeys = keys.each(key => st.object[key])
-                const trueKeys = [];
-                if(st.object["approved"]){
-                  trueKeys.push("approved");
-                }
-                if(st.object["not approved"]){
-                  trueKeys.push("not approved");
-                }
-                
-                
-                let trueKeysLabel = '';
-
-                trueKeys.forEach((key:any, index:any) => {
-                  trueKeysLabel += `${key}${
-                    index === trueKeys.length - 1 ? '' : ', '
-                  }`
-                })
-                return `${
-                  isAllTrue ? 'All' : isAllFalse ? 'None' : `${trueKeysLabel}`
-                }`
-              },
-              verbs: [
-                {
-                  label: 'includes',
-                  value: 'includes',
-                  object: (props:any) => <ApprovedSelectorObject {...props} />,
-                },
-              ],
-            }
-          }}
-        />
-
         <Table
           fullWidth
           schema={defaultSchema}
@@ -394,26 +252,57 @@ const AdminReviews: VtexFunctionComponent = (props: any) => {
             textOf: 'of',
             totalItems: itemsLength,
           }}
+          filters={{
+            alwaysVisibleFilters: ['approved', 'productId', 'locale'],
+            statements: filterStatements,
+            onChangeStatements: handleFiltersChange,
+            clearAllFiltersButtonLabel: 'Clear Filters',
+            collapseLeft: true,
+            options: {
+              approved: {
+                label: 'Approved',
+                renderFilterLabel: (st:any) => {
+                  if (!st || !st.object) {
+                    // you should treat empty object cases only for alwaysVisibleFilters
+                    return 'All'
+                  }
+                  const keys:any = st.object ? Object.keys(st.object) : {}
+                  const isAllTrue = st.object["approved"] && st.object["not approved"]
+                  const isAllFalse = !st.object["approved"] && !st.object["not approved"]
+                  const trueKeys = keys.filter((key:any) => st.object[key])
+                  let trueKeysLabel = ''
+                  trueKeys.forEach((key:any, index:any) => {
+                    trueKeysLabel += `${key}${
+                      index === trueKeys.length - 1 ? '' : ', '
+                    }`
+                  })
+                  return `${
+                    isAllTrue ? 'All' : isAllFalse ? 'None' : `${trueKeysLabel}`
+                  }`
+                },
+                verbs: [
+                  {
+                    label: 'includes',
+                    value: 'includes',
+                    object: approvedSelectorObject,
+                  },
+                ],
+              },
+              productId: {
+                label: 'Product Id',
+                ...simpleInputVerbsAndLabel(),
+              },
+              locale: {
+                label: 'Locale',
+                ...simpleInputVerbsAndLabel(),
+              },
+            },
+          }}
         />
       </div>
     </div>
   )
 }
 
-//export default graphql(adminReviewsQuery)(withRuntimeContext(AdminReviews))
 
-
-export default graphql(adminReviewsQuery, {
-  options: (props:any) => {
-    console.log("LOAD")
-    console.log(props)
-    return ({ variables: { 
-      productId: (props.query && props.query.productId) ? props.query.productId : undefined, 
-      //from: (props.query && props.query.from) ? props.query.from : 0 , 
-      //to:  (props.query && props.query.to) ? props.query.to : 99,
-      //locale: (props.query && props.query.locale) ? props.query.locale : '', 
-      approved: (props.query && props.query.approved) ? props.query.approved : '', 
-      notapproved: (props.query && props.query.notapproved) ? props.query.notapproved : '', 
-    }})
-  }
-})(withRuntimeContext(AdminReviews))
+export default graphql(adminReviewsQuery)(withRuntimeContext(AdminReviews))
